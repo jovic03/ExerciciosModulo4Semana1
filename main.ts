c187cd38681489aef25df4356749baf8ac8a4ef4
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-17 20:52:21 -0300

    Ex 1

---
+++ main.ts
@@ -0,0 +1,21 @@
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Wilker',
+        age: 25,
+        occupation: 'Backend developer'
+    },
+    {
+        name: 'Bob',
+        age: 23,
+        occupation: 'Product Manager'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
